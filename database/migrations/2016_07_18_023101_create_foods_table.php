d05cc464d76d41b8d3e77c3590e4418e13c6ccd6
M. Mnemonic <resil.design@gmail.com>
2016-07-17 22:33:11 -0400

    Adds Food model and migration.

---
+++ database/migrations/2016_07_18_023101_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+
+            $table->integer('protein')->unsigned();
+            $table->integer('carbohydrates')->unsigned();
+            $table->integer('fat')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('foods');
+    }
+}
